285f8c03385a1e12a9b9678cf4ce7f808913ab15
gavin-hyl <gavin.hyl@outlook.com>
2024-04-20 12:03:42 -0700

    Cmake files update.

--- tests/cost_tests.cpp
+++ tests/cost_tests.cpp
@@ -2,15 +2,28 @@
 
 #include <catch2/catch.hpp>
 #include <eigen3/Eigen/Dense>
-#include "../costs/linear_cost.h"
-#include <cstdint>
-
+#include "linear_cost.h"
 
 TEST_CASE("Linear Cost Test") {
-    Eigen::Vector3d v, x1;
-    v << 1, 2, 3;
-    x1 << 1, 1, 1;
-    std::string s = "hi";
-    torc::LinearCost cost = torc::LinearCost<double>(v, s);
-    REQUIRE(cost.Evaluate(x1) == 6);
+    Eigen::Vector3d q1, x1, x2;
+    q1 << 1, 2, 3;
+    x1 << 0.1, 1, 10;
+    x2 << 0, 0, 0;
+    std::string name1 = "linear cost 1";
+    torc::LinearCost cost1 = torc::LinearCost<double>(q1, name1);
+    REQUIRE(cost1.Evaluate(x1) == 6);
+    REQUIRE(cost1.GetDomainDim() == 3);
+    REQUIRE(cost1.GetCoefficients() == q1);
+    REQUIRE(cost1.Gradient() == q1);
+    REQUIRE(cost1.Gradient(x2) == q1);
+    REQUIRE(cost1.Hessian(x1) == Eigen::MatrixXd::Zero(3, 3));
+    REQUIRE(cost1.GetIdentifier() == name1);
+
+    Eigen::VectorXf q2, x3;
+    q2 << 1.5, 2.5;
+    x3 << 4, 8;
+    std::string name2 = "linear cost 2";
+    torc::LinearCost cost2 = torc::LinearCost<float>(q2, name2);
+    REQUIRE(cost2.Evaluate(x3) == 26);
+    REQUIRE(cost2.Gradient() == q2);
 }
