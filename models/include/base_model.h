5a61215c45e785dc101053bd731e208102a00cc0
Zachary <zach.olkin@gmail.com>
2024-05-04 16:42:46 -0700

    initial setup

---
+++ models/include/base_model.h
@@ -0,0 +1,14 @@
+#ifndef TORC_BASE_MODEL_H
+#define TORC_BASE_MODEL_H
+
+namespace torc::models {
+    template <class scalar_t>
+    class BaseModel {
+    public:
+
+    protected:
+    private:
+    };
+} // torc::models
+
+#endif //TORC_BASE_MODEL_H
