46e9782406dd4a3951ef171aef96fdc15e237b6e
Gavin Hua <gavin.hyl@outlook.com>
2024-06-26 20:11:31 -0700

    fixed gitignore

--- cmake-build-release-event-trace/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gavin/caltech/surf2024/torc/tests
-# Build directory: /home/gavin/caltech/surf2024/torc/cmake-build-release-event-trace/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/catch2-build")
