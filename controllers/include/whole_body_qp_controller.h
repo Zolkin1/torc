d067359a3cc5242fea1138a736f5686eddb44128
Zachary <zach.olkin@gmail.com>
2024-07-17 11:00:08 -0700

    started qp controller

---
+++ controllers/include/whole_body_qp_controller.h
@@ -0,0 +1,18 @@
+//
+// Created by zolkin on 7/17/24.
+//
+
+#ifndef TORC_WHOLE_BODY_QP_CONTROLLER_H
+#define TORC_WHOLE_BODY_QP_CONTROLLER_H
+
+namespace torc::controllers {
+    class WholeBodyQPController {
+    public:
+//        WholeBodyQPController()
+    protected:
+    private:
+    };
+}
+
+
+#endif //TORC_WHOLE_BODY_QP_CONTROLLER_H
---
+++ controllers/whole_body_qp_controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zolkin on 7/17/24.
+//
+
+#include "include/whole_body_qp_controller.h"
